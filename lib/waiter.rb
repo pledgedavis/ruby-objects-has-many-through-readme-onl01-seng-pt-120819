8aed3cddfba32b96325ae13fa1f9e73de6039427
Pledge Davis <pledgedavis14@gmail.com>
2020-01-26 22:59:31 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,14 @@
 class Waiter
+  attr_accessor :name, :years_of_experience
+     @@all = []
+  def initialize(name, years_of_experience)
+      @name = name
+      @years_of_experience = years_of_experience
+        @@all << self
+  end
+  def self.all
+
+     @@all
+  end
 
 end
