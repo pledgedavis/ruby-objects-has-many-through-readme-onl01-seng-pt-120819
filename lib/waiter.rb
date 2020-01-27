772caa29132a3e629fc1c04bbaa3d5ce0cc3cee2
Pledge Davis <pledgedavis14@gmail.com>
2020-01-27 02:23:50 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Waiter
   attr_accessor :name, :years_of_experience
      @@all = []
@@ -6,9 +7,38 @@ class Waiter
       @years_of_experience = years_of_experience
         @@all << self
   end
+
   def self.all
 
      @@all
   end
+
+
+  def new_meal(customer, total, tip=0)
+ #  #  Waiter.new(customer, self, total, tip)
+ #  binding.pry
+ # self.new(customer, self, total, tip)
+ newest_meal = Meal.new(self, customer, total, tip)
+   Meal.all << newest_meal
+
+
+  end
+
+def meals
+    # binding.prydef meals
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+  end
+end
+
+def best_tipper
+
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+     best_tipped_meal.customer
+end
+    #  self.length
+
 
 end
